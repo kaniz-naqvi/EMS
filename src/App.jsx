672c944cdeb713afc267368f581ea9b45956ce6b
Mehak <mehak313naqvi@gmail.com>
2025-03-21 07:44:34 +0500

    start over EMS again renew the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Login } from "./components/Auth/Login";
-import { EmployeeBashboard } from "./components/Dashboard/EmployeeBashboard";
-import { Header } from "./components/others/Header";
 export default function App() {
-  return (
-    <>
-      {/*<Login />*/}
-
-      <EmployeeBashboard />
-    </>
-  );
+  return <>hi</>;
 }
