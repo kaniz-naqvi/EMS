5b7d52397b44eaa3e0058043f2cbdd84fc8ee5b4
Mehak <mehak313naqvi@gmail.com>
2025-03-25 14:14:58 +0500

    added edit funtionality

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import TodoReducer from "./todoSlice";
+
 export default store = configureStore({
   reducer: TodoReducer,
 });
--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -7,18 +7,27 @@ const initialState = {
 const todoSlice = createSlice({
   name: "todo",
   initialState,
-  addTodo: (state, action) => {
-    const todo = {
-      id: nanoid(),
-      text: action.payload,
-    };
-    state.todos.push(todo);
-  },
-  editTodo: (state, action) => {
-    const { id, newText } = action.payload;
-    const todo = state.todos.find((todo) => todo.id === id);
-    if (todo) {
-      todo.text = newText;
-    }
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    editTodo: (state, action) => {
+      const { id, newText } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = newText;
+      }
+    },
+    removeTodo: (state, action) => {
+      const id = action.payload;
+      state.todos = state.todos.filter((todo) => todo.id !== id);
+    },
   },
 });
+
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions;
+export default todoSlice.reducer;
