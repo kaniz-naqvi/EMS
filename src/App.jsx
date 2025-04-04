729bc8c30f246a08e55cd2e08de79e50237eb0f6
Mehak <mehak313naqvi@gmail.com>
2025-04-04 12:17:15 +0500

    Starting over EMS

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import React from "react";
-import InputField from "./components/InputField";
-import TodoList from "./components/TodoList";
-import Footer from "./components/Footer";
-import LoginPage from "./components/LoginPage";
-import { useSelector } from "react-redux";
-export default function App() {
-  const isLogin = useSelector((state) => state.user.isLoggedIn);
-  const userName = useSelector((state) => state.user.userName);
-  return (
-    <>
-      <div className="text-center pt-10">
-        {!isLogin && <LoginPage />}
-        {isLogin && (
-          <>
-            <h1 className="text-2xl font-semibold">Welcome {userName}!</h1>
-            <InputField />
-            <TodoList />
-          </>
-        )}
 
-        <Footer />
-      </div>
-    </>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import TodoReducer from "./todoSlice";
-import user from "./userSlice";
-import logger from "./logger";
-const store = configureStore({
-  reducer: { todos: TodoReducer, user: user },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-});
-export default store;
